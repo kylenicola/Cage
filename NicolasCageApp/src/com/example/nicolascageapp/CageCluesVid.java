4424582f3c62353a71f2f6377ae20eeb533680e0
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-12-25 11:56:34 -0600

    making start on redoing the CageCluesVid

--- NicolasCageApp/src/com/example/nicolascageapp/CageCluesVid.java
+++ NicolasCageApp/src/com/example/nicolascageapp/CageCluesVid.java
@@ -1,19 +1,15 @@
 package com.example.nicolascageapp;
-import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.MediaController;
 import android.widget.VideoView;
  
 
 public class CageCluesVid extends Activity
 {
-	//String SrcPath = "http://youtu.be/diQhM7HLNG8";
 	VideoView myVideoView;
 	 
 	   /** Called when the activity is first created. */
@@ -22,21 +18,12 @@ public class CageCluesVid extends Activity
 	   {
 	       super.onCreate(savedInstanceState);
 	      
-	       setContentView(R.layout.cage_clues_video);
+	       setContentView(R.layout.cage_clues);
 	       getActionBar().setDisplayHomeAsUpEnabled(true);
 	       
-	       myVideoView = (VideoView)findViewById(R.id.videoView1);
-	       myVideoView.setMediaController(new MediaController(this));
+	       myVideoView = (VideoView) findViewById(R.id.cage_clues_video);
 	       myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.i_lost_my_hand2));
 	       myVideoView.start();
-	       
-	       /*new Handler().postDelayed(new Runnable() 
-	       {
-	            @Override
-	            public void run() {
-	                setContentView(R.layout.cage_clues_main); 
-	                }
-	            }, 9250);*/
 	   }
 	   
 	   public void moveToQuiz(View view) {
