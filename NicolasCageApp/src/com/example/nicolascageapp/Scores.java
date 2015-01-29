baff9c47f15ef44fe8b91937359d0f3d0685b2e1
Kyle Nicola <kyle.a.nicola@gmail.com>
2015-01-29 17:51:27 -0600

    Making initial start on scores page and updated main_menu_background_color to just standard_color

---
+++ NicolasCageApp/src/com/example/nicolascageapp/Scores.java
@@ -0,0 +1,15 @@
+package com.example.nicolascageapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Scores extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.scores);
+	}
+
+}
