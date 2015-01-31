a562ec7d04de606af3973085ed580f1a8eff41aa
Kyle Nicola <kyle.a.nicola@gmail.com>
2015-01-31 00:33:25 -0600

    added back to menu and replay game options to stats

--- NicolasCageApp/src/com/example/nicolascageapp/Stats.java
+++ NicolasCageApp/src/com/example/nicolascageapp/Stats.java
@@ -5,14 +5,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 public class Stats extends Activity {
-	
+	// for sharedPrefs
 	public static String RATTLETHECAGE_BEST_TIME = "RattleTheCageBestTime";
 	public static String ABCSWITHNIC_BEST_SCORE = "AbcsWithNicBestScore";
 	public static String CAGECLUES_MYSTERY_SOLVED = "CageCluesMysterySolved";
 	public static String CAGECLUES_TIMES_WATCH = "CageCluesTimesWatched";
+	
+	// Where user came from
+	public static int MENU = 1;
+	public static int RATTLETHECAGE = 2;
+	public static int ABCSWITHNIC = 3;
+	public static int CAGECLUES = 4;
 	
 	// stats
 	private long bestTime;
@@ -20,6 +27,7 @@ public class Stats extends Activity {
 	private boolean mysterySolved;
 	private int timesWatched;
 	
+	// views
 	private TextView bestTimeView;
 	private TextView bestScoreView;
 	private TextView mysterySolvedView;
@@ -29,13 +37,19 @@ public class Stats extends Activity {
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
 		setContentView(R.layout.stats);
+		
+		//getActionBar().setDisplayHomeAsUpEnabled(true);
 		
 		// get stats from shared prefs
 		getStats();
 		
 		// set textViews using stats
 		setStats();
+		
+		
 	}
 	
 	private void getStats()
@@ -61,7 +75,7 @@ public class Stats extends Activity {
 			long second = (bestTime / 1000) % 60;
 			long minute = (bestTime / (1000 * 60)) % 60;
 			long hour = (bestTime / (1000 * 60 * 60)) % 24;
-			String time = String.format("%02d:%02d:%02d:%d", hour, minute, second, bestTime);
+			String time = String.format("%02d:%02d:%02d", hour, minute, second);
 			bestTimeView.setText(time);
 		}
 		else
@@ -97,17 +111,21 @@ public class Stats extends Activity {
 			timesWatchedView.setText("NOT ENOUGH");
 		}
 	}
+	
+	//private void 
 
-	public void backPressed(View v)
-	{
-		TextView backView = (TextView) findViewById(R.id.stats_back);
-		backView.setTextColor(android.graphics.Color.YELLOW);
-		onBackPressed();
-	}
+//	public void backPressed(View v)
+//	{
+//		TextView backView = (TextView) findViewById(R.id.stats_back);
+//		backView.setTextColor(android.graphics.Color.YELLOW);
+//		onBackPressed();
+//	}
 	
 	@Override
 	public void onBackPressed() {
 		Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
 		startActivity(intent);
 	}
+	
+	
 }
